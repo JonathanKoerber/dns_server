fc14e31681adf86f5dd5ee191958e175dd9f0492
JonathanKoerber <jonathankoerber@gmail.com>
2023-02-21 13:42:21 -0800

    fixing error b_tree missing colon :(

--- b_tree.py
+++ b_tree.py
@@ -88,7 +88,7 @@ class LinkedBinaryTree(BinaryTree):
     '''Linked repersentation of a binary tree structure'''
     class _Node:
         __slots__ = '_element', '_parent', '_left', '_right'
-        def __init__(self, element, parent='None', left=None, right=None)
+        def __init__(self, element, parent='None', left=None, right=None):
             self._element = element
             self._parent = parent
             self.left = left
