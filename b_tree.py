8976cf99be02920189f812f88ef52c1775e85898
JonathanKoerber <jonathankoerber@gmail.com>
2023-02-23 13:41:24 -0800

    adding working hash map  and b_tree structure

--- b_tree.py
+++ b_tree.py
@@ -1,239 +1,101 @@
-from hash_map import MapBase
-
-class Tree:
-    '''Abstract class representing a tree structure'''
-    class Position:
-        '''An abstraction location of a single element'''
-        def element(self):
-            '''return athe element stored at this Position'''
-            raise NotImplementedError('must be implemented by subclass')
-        def __eq__(self, other):
-            '''Return true if other Position represents the same location'''
-            return not (self == other)
-        
-        def __ne__(self, other):
-            '''return true if other does not represents the same location'''
-            return not (self == other)
-        # ----abstract metods that must be implemented
-        def root(self):
-            '''return postion of tree's root'''
-            raise NotImplementedError('must be implemented by subclass')
-        
-        def parent(self, p):
-            raise NotImplementedError('must be implemented by subclass')
-        def num_children(self, p):
-            raise NotImplementedError('must be implemented by subclass')
-        def __len__(self):
-            raise NotImplementedError('must be implemented by subclass')
-        
-        def is_root(self, p):
-            return self.root() == p
-        
-        def is_leaf(self, p):
-            return self.num_children(p) == 0
-        
-        def is_empty(self):
-            return len(self) == 0 
-        
-        def depth(self, p):
-            '''Reuturn the number of levels seporation Position p from the root'''
-            if self.is_root(p):
-                return 0
-            else:
-                return 1+ self.depth(self.parent())
-        def _height(self, p):
-            '''Return the hieght of the subtree rooted at Postion p.'''
-            if self.is_leaf(p):
-                return 0
-            else: 
-                return 1 + max(self._height(c) for c in self.num_children(p))
-        
-        def height(self, p = None):
-            '''Return the height of the subtree rooted at postion p'''
-            if p is None:
-                p = self._height(p)
-            return self._height(p)
-
-class BinaryTree(Tree):
-    '''Abstract bass class representing a binary tree structure'''
-    def left(self, p): 
-        '''Return a position representing p's left child
-        Return None if p dose not have a left child.'''
-        raise NotImplementedError('must be implemented by subclass')
-    
-    def right(self, p):
-        '''Return  a position representing p right child
-        Return None if p  does not have a right child'''
-        raise NotImplementedError('must be implemented by subclass')
-    # --- concrete methods---
-    def sibling(self, p):
-        '''Return a Position representing p's sibling(or None )'''
-        parent = self.parent(p)
-        if parent is None:
-            return None
-        else:
-            if p == self.left(parent):
-                return self.right(parent)
-            else:
-                return self.left(parent)
-            
-    def children(self, p):
-        '''Generate a iteration of Positons repersenting p's children.'''
-        if self.left(p) is not None:
-            yield self.left(p)
-        if self.right(p) is not None:
-            yield self.right(p)
-        
-class LinkedBinaryTree(BinaryTree):
-    '''Linked repersentation of a binary tree structure'''
-    class _Node:
-        __slots__ = '_element', '_parent', '_left', '_right'
-        def __init__(self, element, parent='None', left=None, right=None):
-            self._element = element
-            self._parent = parent
-            self.left = left
-            self._right = right
-        
-    class Position(BinaryTree.Position):
-        def __init_(self, container, node):
-            '''Constructor should not be invokd by user'''
-            self._container = container
-            self._node = node
-        
-        def element(self):
-            '''Returns element stored at this Position'''
-            return self._node._element
-
-        def __eq__(self, other):
-            '''Return True if other is a Position representing the same location'''        
-            return type(other) is type(self) and other._node is self._node
-        
-    def _validate(self, p):
-        '''Return associated node, if position is valid'''
-        if not isinstance(p, self.Position):
-            raise TypeError('p must be a proper Position type')
-        if p._container is not self:
-            raise ValueError('p does not belong to this container')
-        if p._node._parent is p._node:
-            raise ValueError('p is no longer valid')
-        return p._node
-    
-    def _make_position(self, node):
-            '''Return Position instance of given node or None if no node'''
-            return self.Position(self, node) if node is not None else None
-    
-    # --- Binary tree constuctor
-    def __init__(self):
-        self._root = None
-        self._size = 0
-
-    #--Public accessors
-    def __len__(self):
-        '''Return the number of elements in the tree'''
-        return self._size
-    
-    def root(self):
-        '''Return the root of the Position'''
-        return self._make_position(self._root)
-    
-    def parent(self, p):
-        '''Return the Position of p's parent or None if tree is empty'''
-        node = self._validate(p)
-        return self._make_position(node._parent)
-    
-    def left(self, p):
-        '''Return the Position of p's left child or None if no left child'''
-        node = self._validate(p)
-        return self._make_position(node._left)
-    
-    def right(self, p):
-        '''Return the Position of p's left child or None if no left child'''
-        node = self._validate(p)
-        return self._make_position(node._right)
-    
-    def num_children(self, p):
-        '''Return the number of children o Position p.'''
-        node = self._validate(p)
-        count = 0
-        if node._left is not None:
-            count += 1
-        if node._right is not None:
-            count += 1
-        return count
-    
-    def _add_root(self, e):
-        '''Place element e at the root of an empty tree and return new Position.
-        Raise ValueError if tree non empty'''
-
-        if self._root is not None: raise ValueError('Root exixts')
-        self._size += 1
-        self._root = self._None(e)
-        return self._make_position(self._root)
-    
-    def _add_left(self, p, e):
-        '''Create a new left child for Position p, storing element e
-        Return the Position of new node
-        Raise ValueError if Position p is invalid or p already has a left child'''
-
-        node = self._validate(p)
-        if node._left is not None: raise ValueError('Left child exixts')
-        self._size += 1
-        node._left = self._Node(e, node)
-        return self._make_position(node._left)
-    
-    def _add_right(self, p, e):
-        '''Create a new right child for Position p, storing element e
-        Return the Position of new node
-        Raise ValueError if Position p is invalid or p already has a right child'''
-
-        node = self._validate(p)
-        if node._right is not None: raise ValueError('right child exixts')
-        self._size += 1
-        node._right = self._Node(e, node)
-        return self._make_position(node._right)
-    
-    def _replace(self, p, e):
-        '''Replace the element at position p with e, and return old element'''
-        node = self._validate(p)
-        old = node._element
-        node._element = e
-        return old
-    
-    def _delete(self, p):
-        '''Delete the not at Position p, and replace it with its child, if any
-        Return the element that had been stored at Positin p 
-        Raise ValueError if Position p is invalid or p has two children'''
-        node = self._validate(p)
-        if self.num_children(p) == 2:raise ValueError('p has two children')
-        child = node._left if node._left else node._right
-        if child is not None:
-            child._parrent = node._parent
-        if child is self._root:
-            self._root = child
-        else:
-            parent = node._parent
-            if node is parent._left:
-                parent._left = child
-            else:
-                parent._right = child
-            self._size -= 1
-            node._parent = node
-            return node._element
-        
-        def _attach(self, p, t1, t2):
-            '''Attach trees t1 and t2 as left and right subtree of exernal p'''
-            node = self._validate(p)
-            if not self.is_leaf(p): raise ValueError('position must be leaf')
-            if not type(self) is type(t1) is type(t2):
-                raise TypeError('Tree types must match')
-            self._size += len(t1) + len(t2)
-            if not t1.is_empty():
-                t1._root._parent = node
-                node._left = t1.root
-                t1._root = None
-                t1._size = 0
-            if not t2.is_empty():
-                t2._root._parent = node
-                node._right = t2._root
-                t2._root = None
-                t2._size = 0
+class BTreeNode:
+  def __init__(self, leaf=False):
+    self.leaf = leaf
+    self.keys = []
+    self.child = []
+ 
+ 
+# Tree
+class BTree:
+  def __init__(self, t):
+    self.root = BTreeNode(True)
+    self.t = t
+ 
+    # Insert node
+  def insert(self, k):
+    root = self.root
+    if len(root.keys) == (2 * self.t) - 1:
+      temp = BTreeNode()
+      self.root = temp
+      temp.child.insert(0, root)
+      self.split_child(temp, 0)
+      self.insert_non_full(temp, k)
+    else:
+      self.insert_non_full(root, k)
+ 
+    # Insert nonfull
+  def insert_non_full(self, x, k):
+    i = len(x.keys) - 1
+    if x.leaf:
+      x.keys.append((None, None))
+      while i >= 0 and k[0] < x.keys[i][0]:
+        x.keys[i + 1] = x.keys[i]
+        i -= 1
+      x.keys[i + 1] = k
+    else:
+      while i >= 0 and k[0] < x.keys[i][0]:
+        i -= 1
+      i += 1
+      if len(x.child[i].keys) == (2 * self.t) - 1:
+        self.split_child(x, i)
+        if k[0] > x.keys[i][0]:
+          i += 1
+      self.insert_non_full(x.child[i], k)
+ 
+    # Split the child
+  def split_child(self, x, i):
+    t = self.t
+    y = x.child[i]
+    z = BTreeNode(y.leaf)
+    x.child.insert(i + 1, z)
+    x.keys.insert(i, y.keys[t - 1])
+    z.keys = y.keys[t: (2 * t) - 1]
+    y.keys = y.keys[0: t - 1]
+    if not y.leaf:
+      z.child = y.child[t: 2 * t]
+      y.child = y.child[0: t - 1]
+ 
+  # Print the tree
+  def print_tree(self, x, l=0):
+    print("Level ", l, " ", len(x.keys), end=":")
+    for i in x.keys:
+      print(i, end=" ")
+    print()
+    l += 1
+    if len(x.child) > 0:
+      for i in x.child:
+        self.print_tree(i, l)
+ 
+  # Search key in the tree
+  def search_key(self, k, x=None):
+    if x is not None:
+      i = 0
+      while i < len(x.keys) and k > x.keys[i][0]:
+        i += 1
+      if i < len(x.keys) and k == x.keys[i][0]:
+        return (x, i)
+      elif x.leaf:
+        return None
+      else:
+        return self.search_key(k, x.child[i])
+       
+    else:
+      return self.search_key(k, self.root)
+ 
+ 
+def main():
+  B = BTree(3)
+ 
+  for i in range(10):
+    B.insert((i, 2 * i))
+ 
+  B.print_tree(B.root)
+ 
+  if B.search_key(8) is not None:
+    print("\nFound")
+  else:
+    print("\nNot Found")
+ 
+ 
+if __name__ == '__main__':
+  main()
--- hash_map.py
+++ hash_map.py
@@ -1,157 +1,112 @@
-from collections import MutableMapping
-from random import randrange
-
-class MapBase(MutableMapping):
-    '''Base class to build other maps from'''
-    class _Item:
-        __slots__ = '_key', '_value'
-
-        def __init__(self, k, v):
-            self._key = k
-            self._value = v
-
-        def __eq__(self, other):
-            return self._key == other
-        
-        def __It__(self, other):
-            return self._key < other._key
-        
-class UnsortedTableMap(MapBase):
-    '''Map using an unordered list.'''
-    def __init__(self):
-        self._table = []
-    def __getitem__(self, k):
-        for item in self._table:
-            if k == item._key:
-                return item._value
-        raise KeyError('Key ErrorL '+ repr(k))
-    
-    def __setitem__(self, k, v):
-        for item in self._table:
-            if k == item._key:
-                item._value = v
-                return
-            self._table.append(self._Item(k,v))
-
-    def __delitem__(self, k):
-        for j in range(len(self._table)):
-            if k == self._table[j]._key:
-                self._table.pop(j)
-                return
-        raise KeyError('Key Error: '+ repr(k))
-    
-    def __len__(self):
-        return len(self._table)
-    
-    def __iter__(self):
-        '''Generate iteration of the map's keys'''
-        for item in self._table:
-            yield item._key
-
-class HashMapBase(MapBase):
-    '''Abstract bass class for map uning hash-table with MAD compression'''
-    
-    def __init__(self, cap=11, p=109345121):
-        self._table = cap * [None]
-        self._n = 0
-        self._prime = p
-        self._scale = 1 + randrange(p-1)
-        self._shift = randrange(p)
-
-    def _hash_function(self, k):
-        return  (hash(k)*self._scale+self._shift)%self._prime % len(self._table)
-    
-    def __len__(self):
-        return self._n
-    
-    def __getitem__(self, k):
-        j = self._hash_function(k)
-        return self._bucket_getitem(j,k)
-    
-    def __setitem__(self, k, v):
-        j = self._hash_function(k)
-        self._bucket_setitem(j, k, v)
-        if self._n > len(self._table) // 2:
-            self._resize(2 * len(self._table) -1)
-
-    def __delitem__(self, k):
-        j = self._hash_function(k)
-        self._bucket_delitem(j, k)
-        self._n -1
-    
-    def _resize(self, c):
-        old = list(self.items())
-        self._table = c * [None]
-        self._n = 0
-        for (k,v) in old:
-            self[k] = v
-    
-class ChainHashMap(HashMapBase):
-    '''Hash map implemented with separate chaining for collision resolution'''
-    
-    def _bucket_getitem(self, j, k):
-        bucket = self._table[j]
-        if bucket is None:
-            raise KeyError('Key Error: ' + repr(k))
-        return bucket[k]
-    
-    def _bucket_setitem(self, j, k, v):
-        if self._table[j] is None:
-            self._table[j] = UnsortedTableMap()
-        oldsize = len(self._table[j])
-        self._table[j][k] = v
-        if len(self._table[j]) > oldsize:
-            self._n += 1
-
-    def __iter__(self):
-        for bucket in self._table:
-            if bucket is not None:
-                for key in bucket:
-                    yield key
-
-class ProbeHashMap(HashMapBase):
-    '''Hash map with linear probing for collision resolution'''
-    _AVAIL = object()
-
-    def _is_avalible(self, j):
-        '''True in index j in avalible'''
-        return self._table[j] is None or self._table[j] is ProbeHashMap._AVAIL
-    
-    def _find_slot(self, j, k):
-        '''Search for key k in bucket at index j'''
-
-        firstAvail = None
-        while True:
-            if self._is_avalible(j):
-                if firstAvail is None:
-                    firstAvail = j
-                if self._table[j] is None:
-                    return (False, firstAvail)
-                elif k == self._table[j]._key:
-                    return (True, j)
-                j = (j + 1) % len(self._table)
-    
-    def _bucket_getitem(self, j, k):
-        found, s = self._find_slot(j,k)
-        if not found:
-            raise KeyError('Key Error: ' + repr(k))
-        return self._table[s]._value
-    
-    def _bucket_setitem(self, j, k, v):
-        found, s  = self._find_slot(j,k)
-        if not found:
-            self._table[s] = self._Item(k,v)
-            self.n += 1
+class HashTable:
+ 
+    # Create empty bucket list of given size
+    def __init__(self, size):
+        self.size = size
+        self.hash_table = self.create_buckets()
+ 
+    def create_buckets(self):
+        return [[] for _ in range(self.size)]
+ 
+    # Insert values into hash map
+    def set_val(self, key, val):
+       
+        # Get the index from the key
+        # using hash function
+        hashed_key = hash(key) % self.size
+         
+        # Get the bucket corresponding to index
+        bucket = self.hash_table[hashed_key]
+ 
+        found_key = False
+        for index, record in enumerate(bucket):
+            record_key, record_val = record
+             
+            # check if the bucket has same key as
+            # the key to be inserted
+            if record_key == key:
+                found_key = True
+                break
+ 
+        # If the bucket has same key as the key to be inserted,
+        # Update the key value
+        # Otherwise append the new key-value pair to the bucket
+        if found_key:
+            bucket[index] = (key, val)
         else:
-            self._table[s]._value = v
-
-    def _bucket_delitem(self, j, k):
-        found, s = self._find_slot(j, k)
-        if not found:
-            raise KeyError('Key Error: ' + repr(k))
-        self._table[s] = ProbeHashMap._AVAIL
-    
-    def __iter__(self):
-        for j in range(len(self._table)):
-            if not self._is_avalible(j):
-                yield self._table[j]._key
+            bucket.append((key, val))
+ 
+    # Return searched value with specific key
+    def get_val(self, key):
+       
+        # Get the index from the key using
+        # hash function
+        hashed_key = hash(key) % self.size
+         
+        # Get the bucket corresponding to index
+        bucket = self.hash_table[hashed_key]
+ 
+        found_key = False
+        for index, record in enumerate(bucket):
+            record_key, record_val = record
+             
+            # check if the bucket has same key as
+            # the key being searched
+            if record_key == key:
+                found_key = True
+                break
+ 
+        # If the bucket has same key as the key being searched,
+        # Return the value found
+        # Otherwise indicate there was no record found
+        if found_key:
+            return record_val
+        else:
+            return "No record found"
+ 
+    # Remove a value with specific key
+    def delete_val(self, key):
+       
+        # Get the index from the key using
+        # hash function
+        hashed_key = hash(key) % self.size
+         
+        # Get the bucket corresponding to index
+        bucket = self.hash_table[hashed_key]
+ 
+        found_key = False
+        for index, record in enumerate(bucket):
+            record_key, record_val = record
+             
+            # check if the bucket has same key as
+            # the key to be deleted
+            if record_key == key:
+                found_key = True
+                break
+        if found_key:
+            bucket.pop(index)
+        return
+ 
+    # To print the items of hash map
+    def __str__(self):
+        return "".join(str(item) for item in self.hash_table)
+ 
+ 
+hash_table = HashTable(50)
+ 
+# insert some values
+hash_table.set_val('gfg@example.com', 'some value')
+print(hash_table)
+print()
+ 
+hash_table.set_val('portal@example.com', 'some other value')
+print(hash_table)
+print()
+ 
+# search/access a record with key
+print(hash_table.get_val('portal@example.com'))
+print()
+ 
+# delete or remove a value
+hash_table.delete_val('portal@example.com')
+print(hash_table)
